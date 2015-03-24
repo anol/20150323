6c982bc0cf169f8faf8f7a9687c3ab0a32640fd4
anders <aeolsen@gmail.com>
2015-03-24 15:43:22 +0100

    Adding SSI peripherals

---
+++ aeo1_drivers/src/ssi_peripheral.cpp
@@ -0,0 +1,219 @@
+/*
+ * ssi_peripheral.cpp
+ *
+ *  Created on: 3. mars 2015
+ *      Author: Anders
+ */
+//--------------------------------
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_memmap.h"
+#include "driverlib/rom.h"
+#include "driverlib/rom_map.h"
+#include "driverlib/gpio.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/ssi.h"
+#include "driverlib/sysctl.h"
+#include "utils/uartstdio.h"
+//--------------------------------
+#include "inc/tm4c123gh6pm.h"
+//--------------------------------
+#include "ssi_peripheral.h"
+//--------------------------------
+extern "C" void OnSSI0Interrupt(void) {
+	if (aeo1::ssi_peripheral::m_pTheSSI0) {
+		aeo1::ssi_peripheral::m_pTheSSI0->OnInterrupt();
+	}
+}
+//--------------------------------
+extern "C" void OnSSI1Interrupt(void) {
+	if (aeo1::ssi_peripheral::m_pTheSSI1) {
+		aeo1::ssi_peripheral::m_pTheSSI1->OnInterrupt();
+	}
+}
+//--------------------------------
+extern "C" void OnSSI2Interrupt(void) {
+	if (aeo1::ssi_peripheral::m_pTheSSI2) {
+		aeo1::ssi_peripheral::m_pTheSSI2->OnInterrupt();
+	}
+}
+//--------------------------------
+extern "C" void OnSSI3Interrupt(void) {
+	if (aeo1::ssi_peripheral::m_pTheSSI3) {
+		aeo1::ssi_peripheral::m_pTheSSI3->OnInterrupt();
+	}
+}
+//--------------------------------
+namespace aeo1 {
+//--------------------------------
+ssi_specification SSI0_Specification
+
+= { SYSCTL_PERIPH_GPIOA, // =	GPIOPeripheral
+		GPIO_PORTA_BASE, // = GPIOBase
+		GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5, // = GPIOPins
+		SYSCTL_PERIPH_SSI0, // = SSIPeripheral
+		SSI0_BASE, // = SSIBase
+		GPIO_PA2_SSI0CLK, // = SSIPinClk
+		GPIO_PA3_SSI0FSS, // = SSIPinFss
+		GPIO_PA4_SSI0RX, // = SSIPinRx
+		GPIO_PA5_SSI0TX, // = SSIPinTx
+		0x40008004, //SSI0_CR1_R, // = SSI_CR0_R
+		INT_SSI0 // =	Interrupt
+		};
+//--------------------------------
+ssi_specification SSI1_Specification
+
+= { SYSCTL_PERIPH_GPIOF, // =	GPIOPeripheral
+		GPIO_PORTF_BASE, // = GPIOBase
+		GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, // = GPIOPins
+		SYSCTL_PERIPH_SSI1, // = SSIPeripheral
+		SSI1_BASE, // = SSIBase
+		GPIO_PF2_SSI1CLK, // = SSIPinClk
+		GPIO_PF3_SSI1FSS, // = SSIPinFss
+		GPIO_PF0_SSI1RX, // = SSIPinRx
+		GPIO_PF1_SSI1TX, // = SSIPinTx
+		0x40009004, //SSI1_CR1_R, // = SSI_CR1_R
+		INT_SSI1 // =	Interrupt
+		};
+//--------------------------------
+ssi_specification SSI2_Specification
+
+= { SYSCTL_PERIPH_GPIOB, // =	GPIOPeripheral
+		GPIO_PORTB_BASE, // = GPIOBase
+		GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7, // = GPIOPins
+		SYSCTL_PERIPH_SSI2, // = SSIPeripheral
+		SSI2_BASE, // = SSIBase
+		GPIO_PB4_SSI2CLK, // = SSIPinClk
+		GPIO_PB5_SSI2FSS, // = SSIPinFss
+		GPIO_PB6_SSI2RX, // = SSIPinRx
+		GPIO_PB7_SSI2TX, // = SSIPinTx
+		0x4000A004, //SSI2_CR1_R, // = SSI_CR1_R
+		INT_SSI2 // =	Interrupt
+		};
+
+//--------------------------------
+ssi_specification SSI3_Specification
+
+= { SYSCTL_PERIPH_GPIOD, // = GPIOPeripheral
+		GPIO_PORTD_BASE, // = GPIOBase
+		GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3, // = GPIOPins
+		SYSCTL_PERIPH_SSI3, // = SSIPeripheral
+		SSI3_BASE, // = SSIBase
+		GPIO_PD0_SSI3CLK, // = SSIPinClk
+		GPIO_PD1_SSI3FSS, // = SSIPinFss
+		GPIO_PD2_SSI3RX, // = SSIPinRx
+		GPIO_PD3_SSI3TX, // = SSIPinTx
+		0x4000B004, //SSI3_CR1_R, // = SSI_CR1_R
+		INT_SSI3 // = Interrupt
+		};
+
+//--------------------------------
+ssi_peripheral* ssi_peripheral::m_pTheSSI0 = 0;
+ssi_peripheral* ssi_peripheral::m_pTheSSI1 = 0;
+ssi_peripheral* ssi_peripheral::m_pTheSSI2 = 0;
+ssi_peripheral* ssi_peripheral::m_pTheSSI3 = 0;
+//--------------------------------
+ssi_peripheral::ssi_peripheral(device_id nDevice) :
+		m_nDevice(nDevice),
+		// Use the Right SSI Peripheral Setup
+		m_rSpecification(
+				(ssi_peripheral::SSI0 == nDevice) ?
+						SSI0_Specification :
+						((ssi_peripheral::SSI1 == nDevice) ?
+								SSI1_Specification :
+								((ssi_peripheral::SSI2 == nDevice) ?
+										SSI2_Specification : SSI3_Specification))) {
+}
+//--------------------------------
+ssi_peripheral::~ssi_peripheral() {
+	Terminate();
+}
+//--------------------------------
+void ssi_peripheral::Initialize() {
+	MAP_SysCtlPeripheralEnable(m_rSpecification.m_nSSIPeripheral);
+	MAP_SysCtlPeripheralEnable(m_rSpecification.m_nGPIOPeripheral);
+	MAP_GPIOPinConfigure(m_rSpecification.m_nSSIPinClk);
+	MAP_GPIOPinConfigure(m_rSpecification.m_nSSIPinFss);
+	MAP_GPIOPinConfigure(m_rSpecification.m_nSSIPinRx);
+	MAP_GPIOPinConfigure(m_rSpecification.m_nSSIPinTx);
+	MAP_GPIOPinTypeSSI(m_rSpecification.m_nGPIOBase,
+			m_rSpecification.m_nGPIOPins);
+	// Configure and enable the SSI port for TI master mode.  Use SSI3, system
+	// clock supply, master mode, SSI frequency, and 16-bit data.
+	SSIConfigSetExpClk(m_rSpecification.m_nSSIBase, SysCtlClockGet(),
+	SSI_FRF_MOTO_MODE_2,
+	SSI_MODE_MASTER, 10000, 16);
+	// Enable the SSI module.
+	MAP_SSIEnable(m_rSpecification.m_nSSIBase);
+	// Read any residual data from the SSI port.  This makes sure the receive
+	// FIFOs are empty, so we don't read any unwanted junk.  This is done here
+	// because the TI SSI mode is full-duplex, which allows you to send and
+	// receive at the same time.  The SSIDataGetNonBlocking function returns
+	// "true" when data was returned, and "false" when no data was returned.
+	// The "non-blocking" function checks if there is any data in the receive
+	// FIFO and does not "hang" if there isn't.
+	while (MAP_SSIDataGetNonBlocking(m_rSpecification.m_nSSIBase, &m_nDataRx[0])) {
+	}
+	// Enable the SSI interrupt
+	switch (m_nDevice) {
+	case ssi_peripheral::SSI0:
+		m_pTheSSI0 = this;
+		break;
+	case ssi_peripheral::SSI1:
+		m_pTheSSI1 = this;
+		break;
+	case ssi_peripheral::SSI2:
+		m_pTheSSI2 = this;
+		break;
+	case ssi_peripheral::SSI3:
+		m_pTheSSI3 = this;
+	default:
+		break;
+	}
+	SSIIntDisable(m_rSpecification.m_nSSIBase,
+	SSI_TXFF | SSI_RXFF | SSI_RXTO | SSI_RXOR);
+	SSIIntClear(m_rSpecification.m_nSSIBase,
+	SSI_TXFF | SSI_RXFF | SSI_RXTO | SSI_RXOR);
+	(*((volatile uint32_t *) m_rSpecification.m_nSSI_CR1_R)) |= SSI_CR1_EOT; /* switch tx interrupt to eot int */
+	SSIIntEnable(m_rSpecification.m_nSSIBase, SSI_TXFF); /* SSI_TXFF | SSI_RXFF | SSI_RXTO | SSI_RXOR  */
+	MAP_IntEnable(m_rSpecification.m_nInterrupt);
+}
+//--------------------------------
+void ssi_peripheral::Terminate() {
+	switch (m_nDevice) {
+	case ssi_peripheral::SSI0:
+		m_pTheSSI0 = 0;
+		break;
+	case ssi_peripheral::SSI1:
+		m_pTheSSI1 = 0;
+		break;
+	case ssi_peripheral::SSI2:
+		m_pTheSSI2 = 0;
+		break;
+	case ssi_peripheral::SSI3:
+		m_pTheSSI3 = 0;
+	default:
+		break;
+	}
+	SSIIntDisable(m_rSpecification.m_nSSIBase, SSI_TXFF); /* SSI_TXFF | SSI_RXFF | SSI_RXTO | SSI_RXOR  */
+	MAP_IntDisable(m_rSpecification.m_nInterrupt);
+	MAP_SSIDisable(m_rSpecification.m_nSSIBase);
+}
+//--------------------------------
+void ssi_peripheral::OnInterrupt() {
+	uint32_t nIntStatus = SSIIntStatus(m_rSpecification.m_nSSIBase, false);
+	SSIIntClear(m_rSpecification.m_nSSIBase, nIntStatus);
+	LoadFIFO();
+}
+//--------------------------------
+void ssi_peripheral::LoadFIFO() {
+	int32_t nResult = 1;
+	for (int nIndex = 0; nResult && (BufferSize > nIndex); nIndex++) {
+		nResult = SSIDataPutNonBlocking(m_rSpecification.m_nSSIBase,
+				m_nDataTx[nIndex]);
+	}
+}
+//--------------------------------
+} /* namespace aeo1 */
+//--------------------------------
---
+++ aeo1_drivers/src/ssi_peripheral.h
@@ -0,0 +1,59 @@
+#ifndef ssi_peripheral_h
+#define ssi_peripheral_h
+//--------------------------------
+namespace aeo1 {
+//--------------------------------
+struct ssi_specification {
+	uint32_t m_nGPIOPeripheral;
+	uint32_t m_nGPIOBase;
+	uint32_t m_nGPIOPins;
+	uint32_t m_nSSIPeripheral;
+	uint32_t m_nSSIBase;
+	uint32_t m_nSSIPinClk;
+	uint32_t m_nSSIPinFss;
+	uint32_t m_nSSIPinRx;
+	uint32_t m_nSSIPinTx;
+	uint32_t m_nSSI_CR1_R;
+	uint32_t m_nInterrupt;
+};
+//--------------------------------
+class ssi_peripheral {
+public:
+	enum device_id {
+		SSI0, SSI1, SSI2, SSI3
+	};
+
+public:
+	ssi_peripheral(device_id nDevice);
+	virtual ~ssi_peripheral();
+
+public:
+	virtual void Initialize();
+	virtual void OnInterrupt();
+
+public:
+	static ssi_peripheral* m_pTheSSI0;
+	static ssi_peripheral* m_pTheSSI1;
+	static ssi_peripheral* m_pTheSSI2;
+	static ssi_peripheral* m_pTheSSI3;
+
+private:
+	void Terminate();
+	void LoadFIFO();
+
+protected:
+	enum {
+		BufferSize = 8
+	};
+
+protected:
+	device_id m_nDevice;
+	const ssi_specification& m_rSpecification;
+	uint32_t m_nDataTx[BufferSize];
+	uint32_t m_nDataRx[BufferSize];
+	uint32_t m_nIndex;
+};
+//--------------------------------
+} /* namespace aeo1 */
+//--------------------------------
+#endif /* ssi_peripheral_h */
