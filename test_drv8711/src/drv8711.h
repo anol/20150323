616480b590e773a5f3e85bc8e90ce072af347ac9
anders <aeolsen@gmail.com>
2015-03-26 20:02:43 +0100

    test_drv8711: aeo1_drivers and tiva_utils statis libraries are ok.

---
+++ test_drv8711/src/drv8711.h
@@ -0,0 +1,21 @@
+/*
+ * drv8711.h
+ *
+ *  Created on: 24. mars 2015
+ *      Author: Anders
+ */
+
+#ifndef DRV8711_H_
+#define DRV8711_H_
+
+namespace aeo1 {
+
+class drv8711 {
+public:
+	drv8711();
+	virtual ~drv8711();
+};
+
+} /* namespace aeo1 */
+
+#endif /* DRV8711_H_ */
--- test_drv8711/src/tm4c123gh6pm_startup_ccs.c
+++ test_drv8711/src/tm4c123gh6pm_startup_ccs.c
@@ -1,28 +1,30 @@
 //*****************************************************************************
 //
-// Startup code for use with TI's Code Composer Studio.
+// startup_ccs.c - Startup code for use with TI's Code Composer Studio.
 //
-// Copyright (c) 2011-2014 Texas Instruments Incorporated.  All rights reserved.
+// Copyright (c) 2012-2014 Texas Instruments Incorporated.  All rights reserved.
 // Software License Agreement
 // 
-// Software License Agreement
-//
 // Texas Instruments (TI) is supplying this software for use solely and
 // exclusively on TI's microcontroller products. The software is owned by
 // TI and/or its suppliers, and is protected under applicable copyright
 // laws. You may not combine this software with "viral" open-source
 // software in order to form a larger program.
-//
+// 
 // THIS SOFTWARE IS PROVIDED "AS IS" AND WITH ALL FAULTS.
 // NO WARRANTIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT
 // NOT LIMITED TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
 // A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. TI SHALL NOT, UNDER ANY
 // CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL, OR CONSEQUENTIAL
 // DAMAGES, FOR ANY REASON WHATSOEVER.
+// 
+// This is part of revision 2.1.0.12573 of the EK-TM4C123GXL Firmware Package.
 //
 //*****************************************************************************
 
 #include <stdint.h>
+#include "inc/hw_nvic.h"
+#include "inc/hw_types.h"
 
 //*****************************************************************************
 //
@@ -54,7 +56,13 @@ extern uint32_t __STACK_TOP;
 // External declarations for the interrupt handlers used by the application.
 //
 //*****************************************************************************
-// To be added by user
+extern void SysTickIntHandler(void);
+extern void UARTStdioIntHandler(void);
+extern void OnADCInterrupt(void);
+extern void OnSSI0Interrupt(void);
+extern void OnSSI1Interrupt(void);
+extern void OnSSI2Interrupt(void);
+extern void OnSSI3Interrupt(void);
 
 //*****************************************************************************
 //
@@ -82,15 +90,15 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // Debug monitor handler
     0,                                      // Reserved
     IntDefaultHandler,                      // The PendSV handler
-    IntDefaultHandler,                      // The SysTick handler
+    SysTickIntHandler,                      // The SysTick handler
     IntDefaultHandler,                      // GPIO Port A
     IntDefaultHandler,                      // GPIO Port B
     IntDefaultHandler,                      // GPIO Port C
     IntDefaultHandler,                      // GPIO Port D
     IntDefaultHandler,                      // GPIO Port E
-    IntDefaultHandler,                      // UART0 Rx and Tx
+    UARTStdioIntHandler,                    // UART0 Rx and Tx
     IntDefaultHandler,                      // UART1 Rx and Tx
-    IntDefaultHandler,                      // SSI0 Rx and Tx
+    OnSSI0Interrupt,                      	// SSI0 Rx and Tx
     IntDefaultHandler,                      // I2C0 Master and Slave
     IntDefaultHandler,                      // PWM Fault
     IntDefaultHandler,                      // PWM Generator 0
@@ -100,7 +108,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // ADC Sequence 0
     IntDefaultHandler,                      // ADC Sequence 1
     IntDefaultHandler,                      // ADC Sequence 2
-    IntDefaultHandler,                      // ADC Sequence 3
+    OnADCInterrupt,                      	// ADC Sequence 3
     IntDefaultHandler,                      // Watchdog timer
     IntDefaultHandler,                      // Timer 0 subtimer A
     IntDefaultHandler,                      // Timer 0 subtimer B
@@ -117,7 +125,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // GPIO Port G
     IntDefaultHandler,                      // GPIO Port H
     IntDefaultHandler,                      // UART2 Rx and Tx
-    IntDefaultHandler,                      // SSI1 Rx and Tx
+    OnSSI1Interrupt,                        // SSI1 Rx and Tx
     IntDefaultHandler,                      // Timer 3 subtimer A
     IntDefaultHandler,                      // Timer 3 subtimer B
     IntDefaultHandler,                      // I2C1 Master and Slave
@@ -140,8 +148,8 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // GPIO Port J
     IntDefaultHandler,                      // GPIO Port K
     IntDefaultHandler,                      // GPIO Port L
-    IntDefaultHandler,                      // SSI2 Rx and Tx
-    IntDefaultHandler,                      // SSI3 Rx and Tx
+    OnSSI2Interrupt,                      	// SSI2 Rx and Tx
+    OnSSI3Interrupt,                      	// SSI3 Rx and Tx
     IntDefaultHandler,                      // UART3 Rx and Tx
     IntDefaultHandler,                      // UART4 Rx and Tx
     IntDefaultHandler,                      // UART5 Rx and Tx
