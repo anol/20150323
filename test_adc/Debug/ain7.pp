e37410487b1ef6f1edb65d371dc757a17e1e7297
anders <aeolsen@gmail.com>
2015-07-23 23:18:32 +0200

    Minor push.

---
+++ test_adc/Debug/ain7.pp
@@ -0,0 +1,45 @@
+# FIXED
+
+ain7.obj: ../ain7.cpp
+ain7.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdint.h
+ain7.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdbool.h
+ain7.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/yvals.h
+ain7.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdarg.h
+ain7.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/linkage.h
+ain7.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/_lock.h
+ain7.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdio.h
+ain7.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdarg.h
+ain7.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/string.h
+ain7.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h
+ain7.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h
+ain7.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/rom.h
+ain7.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/rom_map.h
+ain7.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h
+ain7.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/interrupt.h
+ain7.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/uart.h
+ain7.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/adc.h
+ain7.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h
+ain7.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/tm4c1233h6pm.h
+ain7.obj: ../ain7.h
+
+../ain7.cpp: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdint.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdbool.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/yvals.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/_lock.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdio.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/string.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/rom_map.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/adc.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/tm4c1233h6pm.h: 
+../ain7.h: 
---
+++ test_adc/Debug/main.pp
@@ -0,0 +1,53 @@
+# FIXED
+
+main.obj: ../main.cpp
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdint.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdbool.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/yvals.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdarg.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/linkage.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/_lock.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdio.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdarg.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/string.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/math.h
+main.obj: c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/ymath.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/fpu.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/rom.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/rom_map.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/interrupt.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/uart.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/adc.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h
+main.obj: ../drivers/rgb.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/tm4c1233h6pm.h
+main.obj: ../ain7.h
+
+../main.cpp: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdint.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdbool.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/yvals.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/linkage.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/_lock.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdio.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/stdarg.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/string.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/math.h: 
+c:/ti/ccsv6/tools/compiler/arm_5.1.8/include/ymath.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/fpu.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/rom_map.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/adc.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h: 
+../drivers/rgb.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/tm4c1233h6pm.h: 
+../ain7.h: 
