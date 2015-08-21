7efcab92e32ba868b793b56294236564d7eebfcd
anol <aeolsen@gmail.com>
2015-08-21 12:54:31 +0200

    Initial commit.

---
+++ test_qei/Debug/main.pp
@@ -0,0 +1,55 @@
+# FIXED
+
+main.obj: ../main.cpp
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/math.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/ymath.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/float.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/time.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_types.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_hibernate.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/fpu.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/interrupt.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/rom.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/systick.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/uart.h
+main.obj: C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/qei.h
+main.obj: ../utils/uartstdio.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h
+
+../main.cpp: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdint.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdbool.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/yvals.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/_lock.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/math.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/ymath.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/float.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/time.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_types.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_memmap.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_hibernate.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/inc/hw_gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/fpu.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/gpio.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/interrupt.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/pin_map.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/rom.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/sysctl.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/systick.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/uart.h: 
+C:/ti/TivaWare_C_Series-2.1.0.12573/driverlib/qei.h: 
+../utils/uartstdio.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.5/include/stdarg.h: 
---
+++ test_qei/main.cpp
@@ -0,0 +1,76 @@
+//--------------------------------
+#include <stdint.h>
+#include <stdbool.h>
+#include <math.h>
+#include <time.h>
+#include "inc/hw_types.h"
+#include "inc/hw_memmap.h"
+#include "inc/hw_hibernate.h"
+#include "inc/hw_gpio.h"
+#include "driverlib/fpu.h"
+#include "driverlib/gpio.h"
+#include "driverlib/interrupt.h"
+#include "driverlib/pin_map.h"
+#include "driverlib/rom.h"
+#include "driverlib/sysctl.h"
+#include "driverlib/systick.h"
+#include "driverlib/uart.h"
+#include "driverlib/qei.h"
+#include "utils/uartstdio.h"
+//--------------------------------
+#define BIT_RATE	115200
+void ConfigureUART(void) {
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+	GPIOPinConfigure(GPIO_PA0_U0RX);
+	GPIOPinConfigure(GPIO_PA1_U0TX);
+	GPIOPinTypeUART(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1);
+	UARTClockSourceSet(UART0_BASE, UART_CLOCK_PIOSC);
+	UARTStdioConfig(0, BIT_RATE, 16000000);
+}
+//--------------------------------
+#define STRINGIZE_NX(A) #A
+#define STRINGIZE(A) STRINGIZE_NX(A)
+static void PrintProgramInfo() {
+	UARTprintf("\n\n");
+	UARTprintf(STRINGIZE(ProjName) " " __DATE__ " " __TIME__);
+	UARTprintf("\n\n");
+}
+//--------------------------------
+#define APP_SYSTICKS_PER_SEC 1000
+static unsigned int g_nTickCounter = 0;
+extern "C" void SysTickIntHandler(void) {
+	g_nTickCounter++;
+}
+//--------------------------------
+void Initialize() {
+	ROM_FPUEnable();
+	ROM_FPUStackingEnable();
+	ROM_SysCtlClockSet(
+	SYSCTL_SYSDIV_5 | SYSCTL_USE_PLL | SYSCTL_XTAL_16MHZ | SYSCTL_OSC_MAIN);
+	ConfigureUART();
+	SysTickPeriodSet(SysCtlClockGet() / APP_SYSTICKS_PER_SEC);
+	SysTickEnable();
+	SysTickIntEnable();
+	IntMasterEnable();
+}
+//--------------------------------
+void MainLoop() {
+	unsigned int nTickCounter = 0;
+	while (1) {
+		if (nTickCounter != g_nTickCounter) {
+			nTickCounter = g_nTickCounter;
+			if (!(nTickCounter % APP_SYSTICKS_PER_SEC)) {
+				UARTprintf(".");
+			}
+		}
+	}
+}
+//--------------------------------
+int main(void) {
+	Initialize();
+	PrintProgramInfo();
+	MainLoop();
+	return 0;
+}
+//--------------------------------
