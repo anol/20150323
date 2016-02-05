db0ebbec73ba5c6cae1b0b96c7111e11095d690b
aeolsen <aeolsen@gmail.com>
2016-02-05 16:31:13 +0100

    Fixed SW_ROOT - moved it to Window/Preferences

--- solo_stepper/Debug/objects.mk
+++ solo_stepper/Debug/objects.mk
@@ -4,9 +4,5 @@
 
 USER_OBJS :=
 
-<<<<<<< Upstream, based on origin/master
-LIBS := -l"libc.a" -l"C:/Users/aeols/git/20150323/aeo1_drivers/Debug/aeo1_drivers.lib" -l"C:/Users/aeols/git/20150323/tiva_utils/Debug/tiva_utils.lib" -l"C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/ccs/Debug/driverlib.lib"
-=======
-LIBS := -l"libc.a" -l"D:/aeols/git/anol/20150323/aeo1_drivers/Debug/aeo1_drivers.lib" -l"D:/aeols/git/anol/20150323/tiva_utils/Debug/tiva_utils.lib" -l"D:/ti/TivaWare_C_Series-2.1.1.71/driverlib/ccs/Debug/driverlib.lib"
->>>>>>> 626e744 Buildt solo_stepper
+LIBS := -l"libc.a" -l"D:/aeols/git/anol/20150323/aeo1_drivers/Debug/aeo1_drivers.lib" -l"D:/aeols/git/anol/20150323/tiva_utils/Debug/tiva_utils.lib" -l"D:/ti/TivaWare_C_Series-2.1.2.111/driverlib/ccs/Debug/driverlib.lib"
 
--- tiva_utils/Debug/utils/cmdline.pp
+++ tiva_utils/Debug/utils/cmdline.pp
@@ -1,16 +1,6 @@
 # FIXED
 
 utils/cmdline.obj: ../utils/cmdline.cpp
-<<<<<<< Upstream, based on origin/master
-utils/cmdline.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h
-utils/cmdline.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdbool.h
-utils/cmdline.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/yvals.h
-utils/cmdline.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h
-utils/cmdline.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/linkage.h
-utils/cmdline.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/_lock.h
-utils/cmdline.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/string.h
-utils/cmdline.obj: C:/ti/TivaWare_C_Series-2.1.2.111/utils/cmdline.h
-=======
 utils/cmdline.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h
 utils/cmdline.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdbool.h
 utils/cmdline.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/yvals.h
@@ -18,20 +8,9 @@ utils/cmdline.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h
 utils/cmdline.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/linkage.h
 utils/cmdline.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/_lock.h
 utils/cmdline.obj: D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/string.h
-utils/cmdline.obj: D:/ti/TivaWare_C_Series-2.1.1.71/utils/cmdline.h
->>>>>>> 626e744 Buildt solo_stepper
+utils/cmdline.obj: D:/ti/TivaWare_C_Series-2.1.2.111/utils/cmdline.h
 
 ../utils/cmdline.cpp: 
-<<<<<<< Upstream, based on origin/master
-C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdbool.h: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/yvals.h: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/linkage.h: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/_lock.h: 
-C:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/string.h: 
-C:/ti/TivaWare_C_Series-2.1.2.111/utils/cmdline.h: 
-=======
 D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdint.h: 
 D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdbool.h: 
 D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/yvals.h: 
@@ -39,5 +18,4 @@ D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/stdarg.h:
 D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/linkage.h: 
 D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/_lock.h: 
 D:/ti/ccsv6/tools/compiler/ti-cgt-arm_5.2.6/include/string.h: 
-D:/ti/TivaWare_C_Series-2.1.1.71/utils/cmdline.h: 
->>>>>>> 626e744 Buildt solo_stepper
+D:/ti/TivaWare_C_Series-2.1.2.111/utils/cmdline.h: 
