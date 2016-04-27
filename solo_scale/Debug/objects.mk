1a40cba9ea27168403b79271c574f905e820c053
Anders <aeolsen@gmail.com>
2016-04-27 14:47:55 +0200

    Renamed a directory

--- solo_scale/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"libc.a" -l"C:/Users/Anders/git/20150323/aeo1_drivers/Debug/aeo1_drivers.lib" -l"C:/Users/Anders/git/20150323/tiva_utils/Debug/tiva_utils.lib" -l"C:/ti/TivaWare_C_Series-2.1.2.111/driverlib/ccs/Debug/driverlib.lib"
-
